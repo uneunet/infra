8c89acee67b9926f9ae1d382b213f811568d3a36
yank.nvim <yanknvim@gmail.com>
2024-09-17 17:13:27 +0900

    Add wekan

--- main.tf
+++ main.tf
@@ -21,33 +21,6 @@ resource "proxmox_lxc" "cf-tunnel" {
 	}
 }
 
-resource "proxmox_lxc" "vaultwarden" {
-	target_node = "uneunet-main"
-	hostname = "vaultwarden"
-	ostemplate = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
-	password = "password"
-	unprivileged = true
-
-	features {
-		nesting = true
-	}
-
-	cores = 1
-	memory = 512
-
-	rootfs {
-		storage = "local-lvm"
-		size = "4G"
-	}
-
-	network {
-		name = "eno1"
-		bridge = "vmbr0"
-		ip = "192.168.1.32/24"
-		gw = "192.168.1.254"
-	}
-}
-
 resource "proxmox_lxc" "nostr" {
 	target_node = "uneunet-main"
 	hostname = "nostr"
@@ -70,7 +43,7 @@ resource "proxmox_lxc" "nostr" {
 	network {
 		name = "eno1"
 		bridge = "vmbr0"
-		ip = "192.168.1.33/24"
+		ip = "192.168.1.32/24"
 		gw = "192.168.1.254"
 	}
 }
@@ -97,7 +70,7 @@ resource "proxmox_lxc" "metrics" {
 	network {
 		name = "eno1"
 		bridge = "vmbr0"
-		ip = "192.168.1.34/24"
+		ip = "192.168.1.33/24"
 		gw = "192.168.1.254"
 	}
 }
@@ -120,7 +93,7 @@ resource "proxmox_lxc" "tailscale" {
 	network {
 		name = "eno1"
 		bridge = "vmbr0"
-		ip = "192.168.1.35/24"
+		ip = "192.168.1.34/24"
 		gw = "192.168.1.254"
 	}
 }
@@ -138,6 +111,29 @@ resource "proxmox_lxc" "gyokai" {
 	rootfs {
 		storage = "local-lvm"
 		size = "16G"
+	}
+
+	network {
+		name = "eno1"
+		bridge = "vmbr0"
+		ip = "192.168.1.35/24"
+		gw = "192.168.1.254"
+	}
+}
+
+resource "proxmox_lxc" "wekan" {
+	target_node = "uneunet-main"
+	hostname = "wekan"
+	ostemplate = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+	password = "password"
+	unprivileged = true
+
+	cores = 2
+	memory = 512
+
+	rootfs {
+		storage = "local-lvm"
+		size = "4G"
 	}
 
 	network {
